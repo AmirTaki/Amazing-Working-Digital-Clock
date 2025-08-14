46c688152bc0958a6fda83a1a717d2ae6e46e1bb
amir <amirtaki623@gmail.com>
2025-08-14 21:45:46 +0330

    js part 4

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ const hours = document.querySelector("#hours")
 const minutes = document.querySelector("#minutes")
 const seconds = document.querySelector("#seconds")
 const ampm = document.querySelector("#ampm")
+
+
+const h = new Date().getHours();
