3e4ca8dc08a1c0f0edb048e3dfa02d77fb549a1f
amir <amirtaki623@gmail.com>
2025-08-14 21:57:14 +0330

    js part 11

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
+setInterval(() => {
+    const hours = document.querySelector("#hours")
+    const minutes = document.querySelector("#minutes")
+    const seconds = document.querySelector("#seconds")
+    const ampm = document.querySelector("#ampm")
 
-const hours = document.querySelector("#hours")
-const minutes = document.querySelector("#minutes")
-const seconds = document.querySelector("#seconds")
-const ampm = document.querySelector("#ampm")
 
+    let h = new Date().getHours();
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let am  = h > 12 ? "PM" : "AM";
 
-let h = new Date().getHours();
-let m = new Date().getMinutes()
-let s = new Date().getSeconds()
-let am  = h > 12 ? "PM" : "AM";
+    // convert 24hr clock to 12hr clock
 
-// convert 24hr clock to 12hr clock
+    if (h > 12){
+        h -= 12
+    }
 
-if (h > 12){
-    h -= 12
-}
+    // add zero befoer single digit number
 
-// add zero befoer single digit number
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
 
-h = (h < 10) ? "0" + h : h
-m = (m < 10) ? "0" + m : m
-s = (s < 10) ? "0" + s : s
-
-hours.innerHTML = h;
-minutes.innerHTML = m;
-seconds.innerHTML = s;
-ampm.innerHTML = am;
-
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
+} )
