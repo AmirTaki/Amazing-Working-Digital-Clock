76e7be9ac956c01f273a1648edca20ad0d867447
amir <amirtaki623@gmail.com>
2025-08-14 21:47:21 +0330

    js part 6

--- src/main.js
+++ src/main.js
@@ -8,3 +8,7 @@ const ampm = document.querySelector("#ampm")
 const h = new Date().getHours();
 const m = new Date().getMinutes()
 const s = new Date().getSeconds()
+
+hours.innerHTML = h;
+minutes.innerHTML = m;
+seconds.innerHTML = s;
